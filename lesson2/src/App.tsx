da69540c5948af642eb8704186f8c9d880c5e228
songbird <loshasha902gmail.com>
2022-06-22 16:46:59 +0300

    create project

---
+++ lesson2/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ResponsiveAppBar from "./Components/AppBar";
+import {Articles} from "./Components/Articles";
+
+function App() {
+  return (
+      <>
+        <ResponsiveAppBar/>
+        <Articles/>
+      </>
+
+  );
+}
+
+export default App;
